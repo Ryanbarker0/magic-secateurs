ff928de47e1621d8e560c83bac315a76844ae811
Ryanbarker0 <ryangeorgebarker0@gmail.com>
2020-05-26 18:03:10 +0100

    add plugin

---
+++ src/main/java/com/magicsecateurs/MagicSecateursPlugin.java
@@ -0,0 +1,34 @@
+package com.magicsecateurs;
+
+import javax.inject.Inject;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
+
+@PluginDescriptor(
+		name = "Magic Secateurs",
+		description = "Highlight when magic secateurs aren't equipped on a farming run",
+		tags = {"overlay", "skilling"},
+		enabledByDefault = false
+)
+
+public class MagicSecateursPlugin extends Plugin
+{
+	@Inject
+	private MagicSecateursOverlay overlay;
+
+	@Inject
+	private OverlayManager overlayManager;
+
+	@Override
+	public void startUp()
+	{
+		overlayManager.add(overlay);
+	}
+
+	@Override
+	public void shutDown()
+	{
+		overlayManager.remove(overlay);
+	}
+}
--- src/test/java/com/magicsecateurs/MagicSecateursPluginTest.java
+++ src/test/java/com/magicsecateurs/MagicSecateursPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.magicsecateurs;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class MagicSecateursPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(MagicSecateursPlugin.class);
 		RuneLite.main(args);
 	}
 }
